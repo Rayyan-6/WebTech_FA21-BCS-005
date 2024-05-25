2af7129c2f6b35cb478227db761310d15d86cc82
Rayyan-6 <muhd.rayyan.mail@gmail.com>
2024-05-26 02:20:17 +0500

    password now encrypted using bcryptjs

--- Semester Project/Express/express.js
+++ Semester Project/Express/express.js
@@ -1,50 +1,56 @@
 const express = require("express");
 const mongoose = require("mongoose");
 // const cookieParser = require("cookie-parser")
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 // const path = require("path");
 const app = express();
 app.use(express.json());
+
+const bcrypt = require('bcryptjs');
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 var expressLayouts = require("express-ejs-layouts");
+var morgan = require("morgan");
 app.use(expressLayouts);
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const checkAuth = require("./middlewares/checkAuth");
+const isAuthenticated = require("./middlewares/isAuthenticated");
 
 // app.use(require("./middlewares/siteMiddleware"));
-// app.use(require("/middlewares/checkAuth"));
 
-const checkAuth = require('./middlewares/checkAuth');
-const isAuthenticated = require('./middlewares/isAuthenticated');
+const session = require("express-session");
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 10 * 60 * 1000,
+    },
+  })
+);
 
-// app.use(require("./middlewares/siteMiddleware"));
-
-
-
-const session = require('express-session');
-app.use(session({
-  secret: 'your_secret_key', // Replace with a strong secret key
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // Use true if you're serving over HTTPS
-}));
-
+// app.use((req, res, next) => {
+//   if (req.session.user && req.cookies.your_secret_key) {
+//     const userName = req.session.user ? req.session.user.name : "guest";
+//     res.render("homepage", { userName: userName });
+//   }
+//   next();
+// });
 
 app.use(checkAuth);
 
-
 let User = require("./models/User");
-
 
 app.use("/", require("./routes/api/books"));
 app.use("/", require("./routes/api/users"));
 app.use("/", require("./routes/sites/books"));
-
-
-
-
 
 // pages start here
 app.get("/", (req, res) => {
@@ -52,8 +58,7 @@ app.get("/", (req, res) => {
   res.render("homepage", { userName: userName });
 });
 
-
-app.get("/contact-us", isAuthenticated,function (req, res) {
+app.get("/contact-us", isAuthenticated, function (req, res) {
   res.render("contact-us");
 });
 app.get("/signup", function (req, res) {
@@ -63,43 +68,53 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return res.redirect('/');
-    }
-    res.clearCookie('connect.sid'); // Ensure the session cookie is cleared
-    res.redirect('/'); // Redirect to the homepage or login page
-  });
+// app.get("/logout", (req, res) => {
+//   req.session.destroy((err) => {
+//     if (err) {
+//       return res.redirect("/");
+//     }
+//     res.clearCookie("connect.sid"); // Ensure the session cookie is cleared
+//     res.redirect("/");
+//   });
+// });
+
+app.get("/logout", (req, res) => {
+  req.session.user=null;
+    // res.clearCookie("connect.sid"); // Ensure the session cookie is cleared
+    res.redirect("/");
+  
 });
 
 app.get("/apipage", function (req, res) {
   res.render("apipage");
 });
 
-app.get('/errorPage', (req, res) => {
-  res.render('errorPage');
+app.get("/errorPage", (req, res) => {
+  res.render("errorPage");
 });
 
 app.post("/signup", async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   try {
     const existingUser = await User.findOne({ email: req.body.email });
-
-    console.log("Existing user:", existingUser); // Add this line for logging
 
     if (existingUser) {
       console.log("User already exists with email:", req.body.email); // Add this line for logging
       // return res.status(400).send("User already exists");
-      return res.render("errorPage",{ msg: "This user already exists" });
+      return res.render("errorPage", { msg: "This user already exists" });
     }
 
     // Create a new user
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    // const newUser = new User({
+    //   name: req.body.name,
+    //   email: req.body.email,
+    //   password: req.body.password,
+    // });
+
+    const newUser = new User(req.body);
+const salt = await bcrypt.genSalt(10);
+newUser.password = await bcrypt.hash(req.body.password, salt);
+
 
     await newUser.save();
     console.log("User saved:", newUser); // Add this line for logging
@@ -111,28 +126,59 @@ app.post("/signup", async (req, res) => {
 });
 
 // Login with sessions
+// app.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     const existingUser = await User.findOne({ email: email });
+
+//     if (!existingUser) {
+//       return res.redirect("/signup");
+//     }
+
+//     if (existingUser.password !== password) {
+//       return res.render("errorPage",{msg:"Password incorrect"});
+//     }
+
+//     // Store user information in the session
+//     req.session.user = {
+//       id: existingUser._id,
+//       name: existingUser.name,
+//       email: existingUser.email,
+//     };
+
+//     res.redirect("/");
+//   } catch (error) {
+//     console.error("Error during login:", error);
+//     res.status(500).send("Error during login");
+//   }
+// });
+
+
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
 
   try {
-    const existingUser = await User.findOne({ email: email });
+    const existingUser = await User.findOne({ email: req.body.email });
 
     if (!existingUser) {
-      return res.redirect("/signup");
+      return res.render("errorPage",{msg:"User doesn't exist"});
     }
 
-    if (existingUser.password !== password) {
-      return res.redirect('/login');
+    const validPassword = await bcrypt.compare(req.body.password, existingUser.password);
+
+    if (validPassword) {
+      // Store user information in the session
+      req.session.user = {
+        id: existingUser._id,
+        name: existingUser.name,
+        email: existingUser.email,
+      };
+      return res.redirect("/");
+    } else {
+      // Password is invalid
+      console.log("password invalid");
+      return res.render("errorPage",{msg:"Password incorrect"});
     }
-
-    // Store user information in the session
-    req.session.user = {
-      id: existingUser._id,
-      name: existingUser.name,
-      email: existingUser.email
-    };
-
-    res.redirect("/");
   } catch (error) {
     console.error("Error during login:", error);
     res.status(500).send("Error during login");
